38ec6b763f730e4cf6ae760f1e60af6a410625e2
bilalbaraich <khanbilalkhanbaraich1@gmail.com>
2025-01-20 17:29:21 +0500

    Create index.java

---
+++ index.java
@@ -0,0 +1,4 @@
+// JavaScript for button interaction
+document.getElementById("clickMeButton").addEventListener("click", function() {
+    document.getElementById("message").innerText = "You clicked the button!";
+});
